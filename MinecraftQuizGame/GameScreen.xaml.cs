6acab96fe7b73e1fcae23a6de9e51426e80f6fed
MrDanDoodles <bubblegumboioart@gmail.com>
2025-01-24 04:35:51 -0500

    Creating GUI and also Adding Functionality

---
+++ MinecraftQuizGame/GameScreen.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.Json;
+using System.IO;
+
+namespace MinecraftQuizGame
+{
+    /// <summary>
+    /// Interaction logic for GameScreen.xaml
+    /// </summary>
+    public partial class GameScreen : Window
+    {
+        // - - - - VARIABLES - - - - 
+        const string gameQuestionsFileString = @"C:\Users\1211d\Desktop\Computer Science\Personal Projects\C#\Minecraft Quiz Game\Questions_JSON_Python\game_questions.json";
+        static Dictionary<string, string> gameQuestions = new Dictionary<string, string>(); //Dict where all game questions will be held
+
+        //Getting info from JSON
+
+        public GameScreen()
+        {
+            InitializeComponent(); //Dont Move THIS
+
+            // - - - - GAME QUESTIONS - - - - 
+            //Getting info from JSON
+            string jsonData = File.ReadAllText(gameQuestionsFileString);
+            gameQuestions = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonData);
+        }
+
+        // - - - - FUNCTIONS - - - - 
+        static int getRandomNumber()
+        {
+            int questionCount = gameQuestions.Count();
+            Random random = new Random();
+            int number = random.Next(0, questionCount);
+            return number;
+        }
+        static string getNewQuestionKey(int randomNum)
+        {
+            var question = gameQuestions.ElementAt(randomNum);
+            return question.Key;
+        }
+        void getNewQuestion(object sender, RoutedEventArgs e)
+        {
+            string question = getNewQuestionKey(getRandomNumber());
+            questionText.Text = question;
+        }
+    }
+}
---
+++ MinecraftQuizGame/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinecraftQuizGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // - - - - - Creating Menu Animation - - - - - 
+            //Creating Transform Group
+            var transformGroup = new TransformGroup();
+
+            //Creating RotateTransform
+            var rotateTransform = new RotateTransform(-15.969);
+            transformGroup.Children.Add(rotateTransform);
+
+            //Creating ScaleTransform
+            var scaleTransform = new ScaleTransform(1.0, 1.0);
+            transformGroup.Children.Add(scaleTransform);
+
+            //Setting yellowMenuText trasnforms
+            yellowMenuText.RenderTransform = transformGroup;
+            yellowMenuText.RenderTransformOrigin = new Point(0.5, 0.5);
+
+            //Creating the Growing Animation
+            var growAnimation = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 1.25,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true, //Makes object go back to original size
+                RepeatBehavior = RepeatBehavior.Forever //Will always be a loop
+            };
+
+            //Applying the Animation to the yellowMenuText
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, growAnimation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, growAnimation);
+
+
+            // - - - - - QUESTIONS BUTTON - - - - - 
+            string pythonExecutable = "C:\\Users\\1211d\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe";
+            string pythonFile = @"C:\Users\1211d\Desktop\Computer Science\Personal Projects\C#\Minecraft Quiz Game\Questions_JSON_Python\Questions\Questions\Main.py";
+
+            //Creating ProccessStartInfo Object
+            var psi = new ProcessStartInfo
+            {
+                FileName = pythonExecutable,
+                Arguments = pythonFile,
+                RedirectStandardOutput = true, // Redirect output
+                RedirectStandardError = true,  // Redirect error output
+                UseShellExecute = false,       // Required for redirecting
+                CreateNoWindow = true          // Run in background without opening a console window
+            };
+        
+        }
+        // - - - - - FUNCTIONS - - - - - 
+        private void PlayGameClick(object sender, RoutedEventArgs e)
+        {
+            GameScreen gameScreen = new GameScreen(); // Create a new instance of GameScreen
+            gameScreen.Show();                        // Show the new window
+            this.Close();                           // Close the current window
+        }
+    
+    }
+
+    
+
+
+}
